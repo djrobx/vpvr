194f678b974a599994471b7cd8697fdbbde701d7
Rawnei <vikron303@gmaill.com>
2020-06-05 13:05:52 +0200

    AO doesn't work properly and it's a huge performance hog.

--- pin/player.cpp
+++ pin/player.cpp
@@ -4456,7 +4456,8 @@ void Player::Render()
    if (cameraMode)
       UpdateCameraModeDisplay();
 
-   const bool useAO = ((m_dynamicAO && (m_ptable->m_useAO == -1)) || (m_ptable->m_useAO == 1)) && m_pin3d.m_pd3dPrimaryDevice->DepthBufferReadBackAvailable() && (m_ptable->m_AOScale > 0.f);
+   const bool useAO = false;
+   //const bool useAO = ((m_dynamicAO && (m_ptable->m_useAO == -1)) || (m_ptable->m_useAO == 1)) && m_pin3d.m_pd3dPrimaryDevice->DepthBufferReadBackAvailable() && (m_ptable->m_AOScale > 0.f);
 
    PostProcess(useAO && !m_disableAO);
 
